3cdd7400bc01e3ced6b9684336f9d8ebc6563e77
Ana Beatriz de Souza <abeatriz.dev@gmail.com>
2025-04-21 14:08:30 -0300

    fix: simplificando o generate-env.js

--- generate-env.js
+++ generate-env.js
@@ -3,15 +3,10 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-const existingEnv = require('./src/environments/environment.development');
-console.log(existingEnv);
-let defaultEnv = {...existingEnv.environment};
-
-console.log(defaultEnv);
 const envContent = `
 export const environment = {
     production: true,
-    apiUrl: '${process.env.API_URL || defaultEnv.apiUrl}'
+    apiUrl: '${process.env.API_URL}'
 };
 `;
 
